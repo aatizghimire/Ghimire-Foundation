3e0688993fa55f420b7a48ecc029eecdc2f0e103
Aatiz_Ghimire <aatizghm@gmail.com>
2021-02-24 23:42:33 +0545

    sample commit

---
+++ resources/views/layouts/index.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <base href="./">
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <title>{{ config('app.name', 'Laravel') }}</title>
+    <!--Fav Icon -->
+    <link rel="icon" href="{{asset('assets/img/favicon.png')}}" type="image/x-icon"/>
+    <link rel="manifest" href="assets/favicon/manifest.json">
+    <meta name="msapplication-TileColor" content="#ffffff">
+    <meta name="msapplication-TileImage" content="assets/favicon/ms-icon-144x144.png">
+    <meta name="theme-color" content="#ffffff">
+    <!-- Main styles for this application-->
+    <link href="css/style.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css">
+<!-- Custom CSS-->
+
+<!--Fav Icon -->
+<link rel="icon" href="{{asset('assets/img/favicon.ico')}}" type="image/x-icon"/>
+
+<!--Font Awesome CDN-->
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css"
+    integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
+    <link href="{{asset('vendors/@coreui/chartjs/css/coreui-chartjs.css')}}" rel="stylesheet">
+  </head>
